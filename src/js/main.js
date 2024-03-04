37b415124aa9b73a16f0464d8b73e005bc02d855
William Azevedo <william988@live.com>
2024-03-04 15:01:57 -0300

    fix css path

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,7 @@ import changeTheme from "./changeTheme.js";
 const botaoEncrypt = document.querySelector(".encrypt-button");
 const botaoDecrypt = document.querySelector(".decrypt-button");
 
-
 botaoEncrypt.addEventListener("click", () => handleEncrypt());
 botaoDecrypt.addEventListener("click", () => handleDecrypt());
 copy();
-changeTheme()
-
-
+changeTheme();
